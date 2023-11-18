eca5553aeefd2dcc8f1760a78bdb3f41be63e0ef
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-11-18 23:59:37 +0300

    feat(add-word-sheet): open keyboard when shown & improve design

--- app/src/main/java/com/aliernfrog/ensimanager/ui/sheet/AddWordSheet.kt
+++ app/src/main/java/com/aliernfrog/ensimanager/ui/sheet/AddWordSheet.kt
@@ -6,16 +6,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.aliernfrog.ensimanager.R
 import com.aliernfrog.ensimanager.ui.component.AppModalBottomSheet
 import com.aliernfrog.ensimanager.ui.component.ButtonIcon
 import com.aliernfrog.ensimanager.ui.viewmodel.ChatViewModel
@@ -28,9 +34,19 @@ fun AddWordSheet(
     chatViewModel: ChatViewModel = getViewModel(),
     state: SheetState = chatViewModel.addWordSheetState
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     val scope = rememberCoroutineScope()
+    val focusRequester = remember { FocusRequester() }
     val type = chatViewModel.type
     val action = stringResource(type.addWordTitleId)
+
+    LaunchedEffect(state.isVisible) {
+        if (state.isVisible) try {
+            focusRequester.requestFocus()
+            keyboardController?.show()
+        } catch (_: Exception) {}
+    }
+
     AppModalBottomSheet(
         title = action,
         sheetState = state
@@ -39,21 +55,11 @@ fun AddWordSheet(
             value = chatViewModel.addWordInput,
             onValueChange = { chatViewModel.addWordInput = it },
             placeholder = { Text(stringResource(type.addWordPlaceholderId)) },
-            trailingIcon = {
-                AnimatedVisibility(
-                    visible = chatViewModel.addWordInput.isNotEmpty(),
-                    enter = fadeIn() + expandHorizontally(),
-                    exit = fadeOut() + shrinkHorizontally()
-                ) {
-                    IconButton(onClick = { chatViewModel.addWordInput = "" }) {
-                        Icon(
-                            painter = rememberVectorPainter(Icons.Rounded.Clear),
-                            contentDescription = null
-                        )
-                    }
-                }
-            },
-            modifier = Modifier.animateContentSize().fillMaxWidth().padding(8.dp)
+            modifier = Modifier
+                .animateContentSize()
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(focusRequester)
         )
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End),
@@ -62,6 +68,16 @@ fun AddWordSheet(
                 horizontal = 8.dp
             )
         ) {
+            Crossfade(chatViewModel.addWordInput.isNotBlank()) { enabled ->
+                OutlinedButton(
+                    onClick = { chatViewModel.addWordInput = "" },
+                    enabled = enabled
+                ) {
+                    ButtonIcon(rememberVectorPainter(Icons.Default.Clear))
+                    Text(stringResource(R.string.action_clear))
+                }
+            }
+
             Button(
                 onClick = { scope.launch {
                     chatViewModel.addWordFromInput()
