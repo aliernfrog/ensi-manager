148992d4622bd3ab65e1d17c3885d50c16f2c685
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-03-17 22:26:42 +0300

    chore: update app version to v1.10.0 (110000)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.aliernfrog.ensimanager"
         minSdk = 24
         targetSdk = 34
-        versionCode = 19500
-        versionName = "1.9.5"
+        versionCode = 110000
+        versionName = "1.10.0"
         vectorDrawables { useSupportLibrary = true }
     }
 
