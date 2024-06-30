835754bd3c34d7cd54d4033f1e77956576828468
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-06-30 20:28:11 +0300

    fix: set `fetching` to `false` when done

--- app/src/main/java/com/aliernfrog/ensimanager/data/EnsiAPIEndpoint.kt
+++ app/src/main/java/com/aliernfrog/ensimanager/data/EnsiAPIEndpoint.kt
@@ -17,12 +17,14 @@ suspend fun EnsiAPIEndpoint.doRequest(body: JSONObject? = null): HTTPResponse {
     val apiViewModel = getKoinInstance<APIViewModel>()
     apiViewModel.fetching = true
     return withContext(Dispatchers.IO) {
-        WebUtil.sendRequest(
+        val response = WebUtil.sendRequest(
             toUrl = url,
             method = method,
             authorization = if (requiresAuth) apiViewModel.setupAuthorization else null,
             json = body,
             userAgent = apiViewModel.userAgent
         )
+        apiViewModel.fetching = false
+        return@withContext response
     }
 }
