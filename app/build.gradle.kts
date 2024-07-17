822532d0b5bc2ec39a0edf3993ab508e39e85075
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-07-17 09:42:27 +0300

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("com.mikepenz.aboutlibraries.plugin")
 }
 
-val composeMaterialVersion = "1.7.0-beta02"
-val composeMaterial3Version = "1.3.0-beta02"
+val composeMaterialVersion = "1.7.0-beta05"
+val composeMaterial3Version = "1.3.0-beta04"
 val composeCompilerVersion = "1.5.14"
 
 android {
@@ -57,7 +57,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:$composeMaterialVersion")
     implementation("androidx.compose.material:material:$composeMaterialVersion")
@@ -65,12 +65,12 @@ dependencies {
     implementation("androidx.compose.material3:material3:$composeMaterial3Version")
     implementation("androidx.compose.material3:material3-window-size-class:$composeMaterial3Version")
     implementation("io.insert-koin:koin-androidx-compose:3.5.6")
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta02")
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta05")
     implementation("com.mikepenz:aboutlibraries-core:11.2.2")
     implementation("aliernfrog:top-toast-compose:2.0.1-alpha01")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("io.coil-kt:coil-svg:2.6.0")
-    implementation("com.github.jeziellago:compose-markdown:0.5.0")
+    implementation("com.github.jeziellago:compose-markdown:0.5.2")
     implementation("net.engawapg.lib:zoomable:1.6.1")
 }
