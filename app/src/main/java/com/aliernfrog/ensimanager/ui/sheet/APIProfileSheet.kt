7c2cefed7eabe8d26b611482e7d16f6d2b25d85e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-02-23 23:44:22 +0300

    fix(api/profile-sheet): fix invisible icon attempt #1

--- app/src/main/java/com/aliernfrog/ensimanager/ui/sheet/APIProfileSheet.kt
+++ app/src/main/java/com/aliernfrog/ensimanager/ui/sheet/APIProfileSheet.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
@@ -196,13 +197,14 @@ fun APIProfileSheet(
                         Icon(
                             imageVector = Icons.Default.Warning,
                             contentDescription = null,
-                            modifier = Modifier.size(32.dp).padding(
+                            modifier = Modifier.padding(
                                 horizontal = 16.dp, vertical = 8.dp
-                            )
+                            ).size(32.dp)
                         )
                         Text(
                             text = stringResource(R.string.api_ssl_trustNew_notSecure),
-                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                            style = MaterialTheme.typography.bodyMedium
                         )
                     }
                 }
