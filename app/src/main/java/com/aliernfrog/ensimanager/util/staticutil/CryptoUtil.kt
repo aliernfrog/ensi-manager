a2c96a7e49e64d80e8648a4928d4e2a0d7e636f2
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-04-08 17:25:59 +0300

    fix????

--- app/src/main/java/com/aliernfrog/ensimanager/util/staticutil/CryptoUtil.kt
+++ app/src/main/java/com/aliernfrog/ensimanager/util/staticutil/CryptoUtil.kt
@@ -8,6 +8,7 @@ import androidx.annotation.Keep
 import com.aliernfrog.ensimanager.TAG
 import java.security.KeyStore
 import java.security.SecureRandom
+import java.security.spec.ECGenParameterSpec
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
@@ -136,13 +137,16 @@ object CryptoUtil {
     }
 
     fun generateBiometricKey() {
-        val keyGenerator = KeyGenerator.getInstance("RSA", "AndroidKeyStore")
+        val keyGenerator = KeyGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_EC,
+            "AndroidKeyStore"
+        )
 
         val keyGenParameterSpec = KeyGenParameterSpec.Builder(
             BIOMETRIC_KEY_ALIAS,
             KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
         )
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+            .setAlgorithmParameterSpec(ECGenParameterSpec("secp256r1"))
             .setUserAuthenticationRequired(true)
             .setInvalidatedByBiometricEnrollment(true)
             .build()
