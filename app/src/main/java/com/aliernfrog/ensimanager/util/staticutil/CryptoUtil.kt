42f53e3e649edc25c6eb12434189bb38f8b1a480
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-04-08 18:24:42 +0300

    en/decrypt instead of (un)wrapping

--- app/src/main/java/com/aliernfrog/ensimanager/util/staticutil/CryptoUtil.kt
+++ app/src/main/java/com/aliernfrog/ensimanager/util/staticutil/CryptoUtil.kt
@@ -35,14 +35,14 @@ object CryptoUtil {
         cipher.init(Cipher.ENCRYPT_MODE, masterKey, IvParameterSpec(iv))
         val encryptedBytes = cipher.doFinal(string.toByteArray(Charsets.UTF_8))
         val passwordSalt = generateSalt()
-        val passwordWrappedKey = wrapKey(masterKey, deriveKey(password, passwordSalt))
+        val passwordWrappedKey = encryptKey(masterKey, deriveKey(password, passwordSalt))
         return EncryptedData(
             data = Base64.encodeToString(encryptedBytes, Base64.DEFAULT),
             iv = Base64.encodeToString(iv, Base64.DEFAULT),
             salt = Base64.encodeToString(passwordSalt, Base64.DEFAULT),
             passwordWrappedKey = Base64.encodeToString(passwordWrappedKey, Base64.DEFAULT),
             biometricWrappedKey = if (withBiometrics) getBiometricKey()?.let {
-                Base64.encodeToString(wrapKey(masterKey, it), Base64.DEFAULT)
+                Base64.encodeToString(encryptKey(masterKey, it), Base64.DEFAULT)
             } else null
         )
     }
@@ -63,27 +63,27 @@ object CryptoUtil {
             salt = reference.salt,
             passwordWrappedKey = reference.passwordWrappedKey,
             biometricWrappedKey = if (withBiometrics) getBiometricKey()?.let {
-                Base64.encodeToString(wrapKey(masterKey, it), Base64.DEFAULT)
+                Base64.encodeToString(encryptKey(masterKey, it), Base64.DEFAULT)
             } ?: reference.biometricWrappedKey else reference.biometricWrappedKey
         )
     }
 
     fun decryptWithPassword(encryptedData: EncryptedData, password: String): DecryptResult {
-        val masterKey = unwrapKey(
+        val masterKey = decryptKey(
             Base64.decode(encryptedData.passwordWrappedKey, Base64.DEFAULT),
             deriveKey(password, Base64.decode(encryptedData.salt, Base64.DEFAULT))
         )
-        val data = decrypt(encryptedData, masterKey!!)
+        val data = decrypt(encryptedData, masterKey)
         return DecryptResult(data, masterKey)
     }
 
     fun decryptWithBiometrics(encryptedData: EncryptedData): DecryptResult? {
         return getBiometricKey()?.let {
-            val masterKey = unwrapKey(
+            val masterKey = decryptKey(
                 Base64.decode(encryptedData.biometricWrappedKey, Base64.DEFAULT),
                 it
             )
-            val data = decrypt(encryptedData, masterKey!!)
+            val data = decrypt(encryptedData, masterKey)
             DecryptResult(data, masterKey)
         }
     }
@@ -110,16 +110,17 @@ object CryptoUtil {
         return SecretKeySpec(key.encoded, SECRET_KEY_ALGORITHM)
     }
 
-    private fun wrapKey(keyToWrap: SecretKey, wrappingKey: SecretKey): ByteArray {
+    private fun encryptKey(keyToEncrypt: SecretKey, encryptionKey: SecretKey): ByteArray {
         val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
-        cipher.init(Cipher.WRAP_MODE, wrappingKey)
-        return cipher.wrap(keyToWrap)
+        cipher.init(Cipher.ENCRYPT_MODE, encryptionKey)
+        return cipher.doFinal(keyToEncrypt.encoded)
     }
 
-    private fun unwrapKey(wrappedKey: ByteArray, unwrappingKey: SecretKey): SecretKey? {
+    private fun decryptKey(encryptedKey: ByteArray, decryptionKey: SecretKey): SecretKey {
         val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
-        cipher.init(Cipher.UNWRAP_MODE, unwrappingKey)
-        return cipher.unwrap(wrappedKey, SECRET_KEY_ALGORITHM, Cipher.SECRET_KEY) as? SecretKey
+        cipher.init(Cipher.DECRYPT_MODE, decryptionKey)
+        val decryptedKey = cipher.doFinal(encryptedKey)
+        return SecretKeySpec(decryptedKey, SECRET_KEY_ALGORITHM)
     }
 
     private fun generateSalt(): ByteArray {
